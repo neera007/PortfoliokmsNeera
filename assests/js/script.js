33879986caa13942372dbbfc2811016db55be563
neera007 <neeracse007@gmail.com>
2021-12-06 22:02:37 +0600

    js

--- assests/js/script.js
+++ assests/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Portfolio | Jigar Sable";
+        document.title = "Portfolio | kMS Neera";
         $("#favicon").attr("href","assests/images/favicon.png");
     }
     else {
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["front end development", "web designing", "android development", "web development"],
+        strings: [ "android development", "web development","front end development", "web designing"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
@@ -98,15 +98,15 @@ document.onkeydown = function(e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+//var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//(function(){
+//var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//s1.async=true;
+//s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//s1.charset='UTF-8';
+//s1.setAttribute('crossorigin','*');
+//s0.parentNode.insertBefore(s1,s0);
+//})();
 // End of Tawk.to Live Chat
 
 
